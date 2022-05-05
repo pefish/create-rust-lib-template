33836983e65a9d8455ad1e22e2767feb7d5228c6
pefish <pefish@qq.com>
2022-05-05 18:22:33 +0800

    add example

---
+++ examples/test.rs
@@ -0,0 +1,15 @@
+
+extern crate create_rust_lib_template;
+
+use create_rust_lib_template::hello;
+use anyhow::{Result, Ok};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+
+    println!("{}", hello().await);
+    
+    
+    Ok(())
+}
+
